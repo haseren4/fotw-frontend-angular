f75c5163f5dcc817b81685be11a8a15b371ca809
Robert Walk <haseren4@gmail.com>
2025-10-07 16:17:49 -0400

    changed "Browse Forts" button to "Browse Sites" added /getting-started to Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,8 @@
 import { Routes } from '@angular/router';
 import { HomeSummary } from './home-summary/home-summary';
 import { FutureRoadmap } from './future-roadmap/future-roadmap';
-import { NewLocationFormComponent } from './new-location-form/new-location-form'; // ← this file exports the class above
+import { NewLocationFormComponent } from './new-location-form/new-location-form';
+import {GettingStartedComponent} from './getting-started-public/getting-started-public'; // ← this file exports the class above
 
 export const routes: Routes = [
   {
@@ -24,5 +25,10 @@ export const routes: Routes = [
     path: 'roadmap',
     component: FutureRoadmap,
     title: 'Fortifications on the Air | Feature Roadmap'
+  },
+  {
+    path: 'getting-started',
+    component: GettingStartedComponent,
+    title: 'Fortifications on the Air | Getting Started'
   }
 ];
