30bd3b066f98f841a3e2aa27f47e670d3ddb830d
Robert Walk <haseren4@gmail.com>
2025-10-13 18:11:53 -0400

    Minor Content Tweeks (remove mentions of only active duty locations)

--- src/app/getting-started-public/getting-started-public.ts
+++ src/app/getting-started-public/getting-started-public.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {LocationMap} from '../location-map/location-map';
 
 @Component({
   selector: 'app-getting-started',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LocationMap],
   templateUrl: './getting-started-public.html'
 })
 export class GettingStartedComponent {}
