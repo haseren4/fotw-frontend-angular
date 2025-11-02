53d53e562ecac82c152efa0419ee56d2aacfb653
Robert Walk <haseren4@gmail.com>
2025-11-01 22:48:22 -0400

    Update env_vars.ts

--- src/env_vars.ts
+++ src/env_vars.ts
@@ -1,4 +1,4 @@
 export const environment = {
   // Point all backend API requests to local backend server.
-  apiBase: 'http://localhost:8080'
+  apiBase: 'http://ec2-3-88-201-38.compute-1.amazonaws.com:8080'
 };
